b56cb76a0f6eb8e08d71dde3a487e0596690fc8d
vanstee <patrick@vanstee.me>
2016-07-05 17:13:12 -0400

    Get app subcommand working nicely

--- lib/cog_cmd/heroku/app.rb
+++ lib/cog_cmd/heroku/app.rb
@@ -3,8 +3,39 @@ require "heroku/auth"
 
 class CogCmd::Heroku::App < Cog::Command
   def run_command
+    subcommand = request.args.first
+
+    case subcommand
+    when "list", nil
+      list
+    when "info"
+      info
+    end
+  end
+
+  def list
     apps = Heroku::Auth.api.get_apps.body
-    response.content = apps
-    return response
+
+    render(apps)
+  end
+
+  def info
+    return error("You must pass an app of which to show the details") if request.args.length < 2
+
+    app_name = request.args[1]
+    apps = Heroku::Auth.api.get_apps.body
+    app = apps.find { |app| app["name"] == app_name }
+
+    render(app)
+  end
+
+  def render(content)
+    response.content = content
+    response
+  end
+
+  def error(message)
+    response["body"] = message
+    response
   end
 end
